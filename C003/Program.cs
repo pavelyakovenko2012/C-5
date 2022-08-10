d5a65ae8e85aeeac3bef3c5bd28ae70349f1e97a
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-10 15:06:54 +0500

    003

---
+++ C003/Program.cs
@@ -0,0 +1,26 @@
+﻿int firstNumber = 0;
+int secondNumber = 1;
+int result = 0;
+for (int i = 3; i < 10; i++)
+{
+    result = firstNumber+secondNumber;
+    firstNumber=secondNumber;
+    secondNumber=result;
+    System.Console.Write($"{result} ");
+}
+
+
+//  Решение --2
+// int number = 8;
+
+// int firstElement = 0;
+
+// int secondElement = 1;
+// Console.Write(firstElement + "\t" + secondElement + "\t"); // "\t" - Tab - табуляция
+// for (int i = 3; i < number; i++)
+// {
+// int nextElement = firstElement + secondElement;
+// Console.Write(nextElement + "\t");
+// firstElement = secondElement;
+// secondElement = nextElement;
+// }
