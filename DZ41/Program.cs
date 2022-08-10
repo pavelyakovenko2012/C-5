09d76032e63dc9d25e052ccff9f608e8568a002a
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-10 15:07:54 +0500

    DZ41

---
+++ DZ41/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write($"Введите (число), количество чисел (M): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] massiveNumbers = new int[m];
+void InputNumbers(int m)
+{
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введите {i+1} число: ");
+    massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+int Comparison(int[] massiveNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < massiveNumbers.Length; i++)
+  {
+    if(massiveNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+InputNumbers(m);
+Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
